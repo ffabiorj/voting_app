ad364293ce6ae47fbebdb786f7c8eac2cc4b6f23
Fabio Oliveira <fabio20rj@gmail.com>
2020-08-06 22:37:42 -0300

    fixed the file with pep8

--- poll/tests/test_homepage.py
+++ poll/tests/test_homepage.py
@@ -1,8 +1,9 @@
 import pytest
 from django.urls import reverse
 
+
 @pytest.mark.django_db
 def test_home_status_code_200(client):
-    url = reverse('home')
+    url = reverse("home")
     response = client.get(url)
     assert response.status_code == 200
--- poll/tests/test_model.py
+++ poll/tests/test_model.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_user_create():
-    User.objects.create_user('Fabio', 'fabio@gmail.com', '12345667')
+    User.objects.create_user("Fabio", "fabio@gmail.com", "12345667")
     assert User.objects.count() == 1
