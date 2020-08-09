dd003f97bd734e941e121528aafca5f64b4333ae
Fabio Oliveira <fabio20rj@gmail.com>
2020-08-09 10:30:53 -0300

    created view and template vote.

--- poll/urls.py
+++ poll/urls.py
@@ -4,4 +4,6 @@ from poll import views as view
 urlpatterns = [
     path('', view.home, name='home'),
     path('create/', view.create, name='create'),
+    path('vote/<int:pk>/', view.vote, name='vote'),
+
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from poll.models import Poll
 from poll.forms import CreatePollForm
+from django.http import HttpResponse
 
 
 def home(request):
@@ -21,3 +22,27 @@ def create(request):
 
     context = {"form": form}
     return render(request, "create.html", context)
+
+def vote(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    
+    if request.method == "POST":
+        selected_option = request.POST['poll']
+        print(selected_option)
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option2_two_count += 1
+        elif selected_option == 'option3':
+            poll.option2_three_count
+        else:
+            return HttpResponse(400, 'Invalid form')
+
+        poll.save()
+
+        return redirect('home')
+
+    context = {
+        'poll': poll
+    }
+    return render(request, 'vote.html', context)
